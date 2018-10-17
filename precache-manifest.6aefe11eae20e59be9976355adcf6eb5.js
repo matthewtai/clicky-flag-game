9ceaa9666130c146a59351ebdda3f2618d175836
wienerbrod <matthewtai@gmail.com>
2018-10-17 02:07:01 -0400

    Updates

---
+++ precache-manifest.6aefe11eae20e59be9976355adcf6eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-flag-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cbf361e6df1c2223634c",
+    "url": "/clicky-flag-game/static/js/runtime~main.cbf361e6.js"
+  },
+  {
+    "revision": "804185908e1b12d286fc",
+    "url": "/clicky-flag-game/static/js/main.80418590.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-flag-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "804185908e1b12d286fc",
+    "url": "/clicky-flag-game/static/css/main.99024b72.chunk.css"
+  },
+  {
+    "revision": "c8457c13b7a9687f15423108e7847357",
+    "url": "/clicky-flag-game/index.html"
+  }
+];
